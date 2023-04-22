674a75bb1f107beced7ffa287ef73ae0e703b607
Shankar9698 <shankarkamalapur6@gmail.com>
2023-04-22 14:40:45 +0530

    First commit

---
+++ src/main/java/org/example/HibernateUtils.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class HibernateUtils {
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory(){
+        if(sessionFactory==null){
+            Configuration configuration=new Configuration();
+          Properties properties=new Properties();
+            properties.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
+            properties.put(Environment.URL,"jdbc:mysql://localhost:3306/my_db3");
+            properties.put(Environment.USER,"root");
+            properties.put(Environment.PASS,"root");
+            properties.put(Environment.DIALECT,"org.hibernate.dialect.MySQL8Dialect");
+            properties.put(Environment.HBM2DDL_AUTO,"update");
+            properties.put(Environment.SHOW_SQL,"true");
+            configuration.setProperties(properties);
+            ServiceRegistry serviceRegistry=new StandardServiceRegistryBuilder().applySettings(sessionFactory.getProperties()).build();
+            sessionFactory=configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
+    }
+
+
+}
